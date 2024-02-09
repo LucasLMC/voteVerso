6694f244e302316503ad9275d09522af8635c106
Lucas Costa <lucasmlc31@gmail.com>
2024-02-09 13:41:52 -0300

    add poll options

--- src/http/routes/create-polls.ts
+++ src/http/routes/create-polls.ts
@@ -7,15 +7,24 @@ export async function createPoll(app: FastifyInstance) {
     app.post('/polls', async (request, reply) => {
         const createPoolBody = z.object({
             title: z.string(),
+            options: z.array(z.string()),
         })
 
-        const  {title} = createPoolBody.parse(request.body)
-        // return response.status(201).send();
+        const  {title, options} = createPoolBody.parse(request.body)
+
         const poll = await prisma.poll.create({
             data: {
                 title,
+                options: {
+                    createMany: {
+                        data: options.map(option => ({
+                            title: option
+                        }))
+                    }
+                }
             }
         })
-       return reply.status(201).send({ pollId : poll.id });
+
+       return reply.status(201).send({ pollId : poll, options });
     });
 }
