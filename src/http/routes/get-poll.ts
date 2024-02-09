46f264c1d3dcc81a1f96a201f6835a406f6c517c
Lucas Costa <lucasmlc31@gmail.com>
2024-02-09 20:01:38 -0300

    add score in get poll

--- src/http/routes/get-poll.ts
+++ src/http/routes/get-poll.ts
@@ -1,6 +1,7 @@
 import { prisma } from "../../lib/prisma";
 import { z } from 'zod';
 import { FastifyInstance } from "fastify";
+import { redis } from "../../lib/redis";
 
 export async function getPoll(app: FastifyInstance) {
 
@@ -9,7 +10,7 @@ export async function getPoll(app: FastifyInstance) {
             pollId: z.string().uuid(),
         })
 
-        const  {pollId} = createPoolParams.parse(request.params)
+        const { pollId } = createPoolParams.parse(request.params)
 
         const poll = await prisma.poll.findUnique({
             where: {
@@ -18,13 +19,37 @@ export async function getPoll(app: FastifyInstance) {
             include: {
                 options: {
                     select: {
-                        id:true,
-                        title:true
+                        id: true,
+                        title: true
                     }
                 }
             }
         })
 
-       return reply.status(200).send({ poll });
+        if (!poll) {
+            return reply.status(404).send({ message: 'Poll not found' })
+        }
+
+        const result = await redis.zrange(pollId, 0, -1, 'WITHSCORES')
+
+        const votes = result.reduce((obj, line, index) => {
+            if (index % 2 === 0) {
+                return { ...obj, [line]: Number(result[index + 1]) }
+            }
+            return obj
+        }, {} as Record<string, number>)
+
+
+        return reply.status(200).send({
+            poll : {
+                id : poll.id,
+                title : poll.title,
+                options : poll.options.map(option => ({
+                    id : option.id,
+                    title : option.title,
+                    score : (option.id in votes) ? votes[option.id] : 0
+                }))
+            }
+        });
     });
 }
