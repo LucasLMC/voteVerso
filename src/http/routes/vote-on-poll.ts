eac664abdcb7ed3950b826b894807eb934066862
Lucas Costa <lucasmlc31@gmail.com>
2024-02-09 14:53:10 -0300

    validation on votes

--- src/http/routes/vote-on-poll.ts
+++ src/http/routes/vote-on-poll.ts
@@ -1,6 +1,7 @@
 import { prisma } from "../../lib/prisma";
 import { z } from 'zod';
 import { FastifyInstance } from "fastify";
+import { randomUUID } from "crypto";
 
 export async function voteOnPoll(app: FastifyInstance) {
 
@@ -13,10 +14,55 @@ export async function voteOnPoll(app: FastifyInstance) {
             pollId: z.string().uuid(),
         })
 
-        const  { pollOptionId } = voteOnPollBody.parse(request.body)
-        const  { pollId } = voteOnPollParams.parse(request.params)
+        const { pollOptionId } = voteOnPollBody.parse(request.body)
+        const { pollId } = voteOnPollParams.parse(request.params)
 
+        let { sessionId } = request.cookies
 
-       return reply.status(201).send();
+        if (sessionId) {
+            const userPreviousVoteOnPoll = await prisma.votes.findUnique({
+                where: {
+                   sessionId_pollId: {
+                       sessionId,
+                       pollId
+                   }
+                }
+            })
+
+            if (userPreviousVoteOnPoll && userPreviousVoteOnPoll.pollOptionId !== pollOptionId) {
+                await prisma.votes.delete({
+                    where: {
+                        id: userPreviousVoteOnPoll.id
+                    }
+                })
+            } else if(userPreviousVoteOnPoll){
+                return reply.status(409).send({
+                    message: 'You already voted on this poll'
+                })
+            }
+        }
+
+        if (!sessionId) {
+            sessionId = randomUUID()
+
+            reply.setCookie('sessionId', sessionId, {
+                path: '/',
+                maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days
+                signed: true,
+                httpOnly: true
+            })
+        }
+
+        await prisma.votes.create({
+            data: {
+                sessionId,
+                pollId,
+                pollOptionId
+            }
+        })
+
+        return reply.status(201).send({
+            sessionId
+        });
     });
 }
