994da80f05345d482c214c03d143c17ab0f9707f
Lucas Costa <lucasmlc31@gmail.com>
2024-02-09 12:02:29 -0300

    add README and zod to validations

--- src/http/server.ts
+++ src/http/server.ts
@@ -3,7 +3,12 @@ import fastify from 'fastify';
 const app = fastify();
 
 app.get('/hello', () => {
-    return 'Welcome to NLW Expert!';
+    return 'Welcome to NLW Expert Project !';
+});
+
+app.post('/polls', async (request, response) => {
+    // return response.status(201).send();
+   return request.body.title
 });
 
 app.listen({
@@ -11,3 +16,4 @@ app.listen({
 }).then(() => {
     console.log('HTTP server running on http://localhost:3333');
 });
+
