5cd0d22a210dff5ec0f559e9e377fa4df93690d6
Lucas Costa <lucasmlc31@gmail.com>
2024-02-09 12:10:01 -0300

    create save poll metod

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,14 +1,28 @@
 import fastify from 'fastify';
+import  { z } from 'zod';
+import { PrismaClient } from '@prisma/client';
 
 const app = fastify();
+
+const prisma = new PrismaClient();
 
 app.get('/hello', () => {
     return 'Welcome to NLW Expert Project !';
 });
 
-app.post('/polls', async (request, response) => {
+app.post('/polls', async (request, reply) => {
+    const createPoolBody = z.object({
+        title: z.string(),
+    })
+
+    const  {title} = createPoolBody.parse(request.body)
     // return response.status(201).send();
-   return request.body.title
+    const poll = await prisma.poll.create({
+        data: {
+            title,
+        }
+    })
+   return reply.status(201).send({ pollId : poll.id });
 });
 
 app.listen({
